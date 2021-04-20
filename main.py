ba04696387c168f3026b0eee1b3d165b2f26b9e8
Namz <manit.op@outlook.com>
2021-04-20 13:29:41 +0530

    No Novice Devs allowed

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import datetime
 import os
 # import sqlite3
 import database
+import random
 
 
 
@@ -37,24 +38,52 @@ def notfunny(linklol):
     #     return render_template('notfound.html')
     # return redirect(thelink)
 
+@app.route("/hehe", methods=['POST', 'GET'])
+@limiter.limit("1/day;15/month")
+def hehebruv():
+    if request.method == "POST":
+        abc = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "w", "x", "y", "z"]
+        cap_abc = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "W", "X", "Y", "Z"]
+        k = random.randint(1,9)
+        kk = random.randint(11, 19)
+        kkk = random.randint(21, 29)
+        kkkk = random.randint(31, 39)
+        number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+        random_abc_small = random.choice(abc)
+        random_abc_cap = random.choice(cap_abc)
+        open("uwu.txt", 'w').write(f'{random_abc_small}{k}{kkkk}{kkk}{random_abc_cap}{kk}')
+        return f"{random_abc_small}{k}{kkkk}{kkk}{random_abc_cap}{kk}"
+    else:
+        return "You need to POST not GET and btw you exhausted your 1 request of today. Try Again Tommorow!"
+
 @app.route('/create',methods = ['POST', 'GET'])
 def createforthem():
     if request.method == "POST":
-        
+
         name=request.form.get('name')
         url=request.form.get('url')
-        if 'https://discord.com' in url:
-            owo = database.get_link(url)
-            if owo == "Not Found":
-                uwu = database.make_new_link(name, url)
-                if uwu == "Done!":
-                    return redirect("/success")
+        key = request.form.get('key')
+        f = open("uwu.txt", 'r')
+        # f.close()
+        if key == f.read():
+            f.close()
+            k = open('uwu.txt', 'w')
+            k.write('barryisgay')
+            k.close()
+            if 'https://discord.com' in url:
+                owo = database.get_link(url)
+                if owo == "Not Found":
+                    uwu = database.make_new_link(name, url)
+                    if uwu == "Done!":
+                        return redirect("/success")
+                    else:
+                        return redirect('/success?error=error')
                 else:
-                    return redirect('/success?error=error')
+                    return redirect('/success?error=taken')
             else:
-                   return redirect('/success?error=taken')
+                return redirect("/success?error=notdiscord")
         else:
-            return redirect("/success?error=notdiscord")
+            return redirect('/success?error=invalidkey')
         """
         try:
             c.execute("SELECT * FROM links WHERE name={}".format(str(name)))
@@ -90,6 +119,8 @@ def yay():
             return "Some Unknown Error occured while creating it!"
         elif error == "notdiscord":
             return "That was not a discord link bruv!"
+        elif error=="invalidkey":
+            return "The key you entered was invalid"
         else:
             return " An error is encountered!"
     else:
