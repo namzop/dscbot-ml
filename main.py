3c007c06cfc0106576959d78ec59e74ad8031ea3
Namz <notnamz@outlook.com>
2021-04-27 21:40:58 +0530

    Fixed it & changed it back to normal

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 from flask import Flask, request, send_file, send_from_directory, render_template, redirect
-from flask_limiter import Limiter
-from flask_limiter.util import get_remote_address
 import json
 import time
 import datetime
@@ -12,15 +10,11 @@ import random
 
 
 
-app = Flask(__name__)
-limiter = Limiter(
-    app,
-    key_func=get_remote_address
-)
-
 @app.route('/')
 def lol():
     return render_template('vanity.html')
+
+
 
 @app.route('/<linklol>')
 def notfunny(linklol):
@@ -29,83 +23,26 @@ def notfunny(linklol):
         return render_template('notfound.html')
     else:
         return redirect(k)
-    # f=open('data.json')
-    # thedata = json.load(f)
-    # try:
-    #     thefinallink=thedata[linklol][0]['url']
-    #     return redirect(thefinallink)
-    # except:
-    #     return render_template('notfound.html')
-    # return redirect(thelink)
 
-@app.route("/hehe", methods=['POST', 'GET'])
-@limiter.limit("1/day;15/month")
-def hehebruv():
-    if request.method == "POST":
-        abc = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "w", "x", "y", "z"]
-        cap_abc = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "W", "X", "Y", "Z"]
-        k = random.randint(1,9)
-        kk = random.randint(11, 19)
-        kkk = random.randint(21, 29)
-        kkkk = random.randint(31, 39)
-        number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-        random_abc_small = random.choice(abc)
-        random_abc_cap = random.choice(cap_abc)
-        open("uwu.txt", 'w').write(f'{random_abc_small}{k}{kkkk}{kkk}{random_abc_cap}{kk}')
-        return f"{random_abc_small}{k}{kkkk}{kkk}{random_abc_cap}{kk}"
-    else:
-        return "You need to POST not GET and btw you exhausted your 1 request of today. Try Again Tommorow!"
 
 @app.route('/create',methods = ['POST', 'GET'])
 def createforthem():
     if request.method == "POST":
-
         name=request.form.get('name')
         url=request.form.get('url')
         key = request.form.get('key')
-        f = open("uwu.txt", 'r')
-        # f.close()
-        if key == f.read():
-            f.close()
-            k = open('uwu.txt', 'w')
-            k.write('barryisgay')
-            k.close()
-            if 'https://discord.com' in url:
-                owo = database.get_link(url)
-                if owo == "Not Found":
-                    uwu = database.make_new_link(name, url)
-                    if uwu == "Done!":
-                        return redirect("/success")
-                    else:
-                        return redirect('/success?error=error')
+        if 'https://discord.com' in url:
+            owo = database.get_link(url)
+            if owo == "Not Found":
+                uwu = database.make_new_link(name, url)
+                if uwu == "Done!":
+                    return redirect("/success")
                 else:
-                    return redirect('/success?error=taken')
+                    return redirect('/success?error=error')
             else:
-                return redirect("/success?error=notdiscord")
+                return redirect('/success?error=taken')
         else:
-            return redirect('/success?error=invalidkey')
-        """
-        try:
-            c.execute("SELECT * FROM links WHERE name={}".format(str(name)))
-            k = c.fetchone()
-            return k
-            # return "This name is already taken!"
-        except:
-            c.execute(f'INSERT INTO links VALUES ("lmao", "lmfao")')
-            return "Thanks for using !"
-        """
-        # try:
-        #     f = open('data.json', 'r')
-        #     da_guy = json.loads(f.read())
-        #     kk = da_guy[name]
-        #     return redirect('/success?success=false')
-        # except:
-        #     k = open('requests.txt', 'a')
-        #     k.write("Name: {} URL: {} \n".format(name,url))
-        #     k.close()
-        #     return redirect('/success')
-
-
+            return redirect("/success?error=notdiscord")
     else:
         return render_template('register.html')
 
